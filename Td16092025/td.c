af5c88b76c4cdb02730d7bc2045ff5b16aae89c6
Giacchi-M24 <matteo.giacchi@hes-so.ch>
2025-09-16 12:35:12 +0000

    TD16092025

---
+++ Td16092025/td.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_Students = 23;
+	int accound_value = -42;             //ajout du texte signed optionnel
+	float temperature = 18.1;            //32bit
+	double pi = 3.141592653589793;       //64bit
+}
